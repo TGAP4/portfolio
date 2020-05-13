54775841babaa4494a889340b1ca0a7d22be6492
TGAP4 <60671900+TGAP4@users.noreply.github.com>
2020-05-13 19:00:17 -0400

    removed less-impressive projects from list

--- src/containers/projects/project-data/projects-list.jsx
+++ src/containers/projects/project-data/projects-list.jsx
@@ -1,18 +1,18 @@
 export default [
-  {
-    title: `Star Wars Tinder`,
-    description: `Simple React app styled after Tinder that fetches and displays user data from swapi.io.`,
-    repoLink: `https://github.com/TGAP4/star-wars-tinder`,
-    liveLink: `https://tgap4.github.io/star-wars-tinder`,
-    thumbnail: require('./thumbnails/starwarstinder.png')
-  },
-  {
-    title: `Robo Friends`,
-    description: `Simple app using React and Redux that fetches a list of users, sorts by name (removing titles), and allows the user to filter the list by name search.`,
-    repoLink: `https://github.com/TGAP4/robo-friends`,
-    liveLink: `https://tgap4.github.io/robo-friends`,
-    thumbnail: require('./thumbnails/robofriends.png')
-  },
+  // {
+  //   title: `Star Wars Tinder`,
+  //   description: `Simple React app styled after Tinder that fetches and displays user data from swapi.io.`,
+  //   repoLink: `https://github.com/TGAP4/star-wars-tinder`,
+  //   liveLink: `https://tgap4.github.io/star-wars-tinder`,
+  //   thumbnail: require('./thumbnails/starwarstinder.png')
+  // },
+  // {
+  //   title: `Robo Friends`,
+  //   description: `Simple app using React and Redux that fetches a list of users, sorts by name (removing titles), and allows the user to filter the list by name search.`,
+  //   repoLink: `https://github.com/TGAP4/robo-friends`,
+  //   liveLink: `https://tgap4.github.io/robo-friends`,
+  //   thumbnail: require('./thumbnails/robofriends.png')
+  // },
   {
     title: `To-Do Prioritizer`,
     description: `Allows user to create a to-do list that orders tasks based on priority, as well as keeps track of how long the the task has been unfinished.`,
@@ -20,13 +20,13 @@ export default [
     liveLink: `https://tgap4.github.io/to-do-list/`,
     thumbnail: require('./thumbnails/todoprioritizer.png')
   },
-  {
-    title: `Face Recognition API`,
-    description: `Server-side web API for FaceRecognition using Express.js.`,
-    repoLink: `https://github.com/TGAP4/face-recognition-server`,
-    liveLink: null,
-    thumbnail: require('./thumbnails/facerecognitionapi.png')
-  },
+  // {
+  //   title: `Face Recognition API`,
+  //   description: `Server-side web API for FaceRecognition using Express.js.`,
+  //   repoLink: `https://github.com/TGAP4/face-recognition-server`,
+  //   liveLink: null,
+  //   thumbnail: require('./thumbnails/facerecognitionapi.png')
+  // },
   {
     title: `Face Recognition App`,
     description: `React app that allows users to register/sign-in and submit image URLs, using the Clarifai face detection API to detect faces.`,
@@ -36,7 +36,7 @@ export default [
   },
   {
     title: `Crown Clothing E-Commerce Store`,
-    description: `Sample E-commerce site utilizing React, Redux, Redux-Saga, Styled-Components, Firebase, Stripe, and more.`,
+    description: `Online clothing store built from scratch using React, Redux, Redux-Saga, Styled-Components, Google Firebase, Stripe, and more.`,
     repoLink: `https://github.com/TGAP4/crown-clothing`,
     liveLink: `https://strawberry-crumble-12957.herokuapp.com`,
     thumbnail: require('./thumbnails/crownclothing.png')
